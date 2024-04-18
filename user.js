11a02bf745af05416b110d887f49f8ca6feac70c
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-18 23:57:39 +0200

    ADD acceder API

---
+++ user.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(name, image, species) {
+        this.name = name;
+        this.image = image;
+        this.species = species;
+    }
+
+    getname () {
+        return this.name;
+    }
+
+    getimage() {
+        return this.image;
+    }
+
+    getspecies() {
+        return this.species;
+    }
+}
